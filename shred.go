ec4ae96704f98033deb97d656efbf6c4c249f204
Hassam Sulehria <sulehria.hassam@gmail.com>
2025-04-03 08:11:47 -0400

    Quit panicking (pun intended) so tests could function

--- shred.go
+++ shred.go
@@ -6,18 +6,12 @@ import (
 	"os"
 )
 
-
-// Adapted from https://gobyexample.com/reading-files
-func check(e error) {
-	if e != nil {
-		panic(e)
-	}
-}
-
 // Shred overwrites the file at the given path with random data and then deletes it.
 func Shred(path string) error {	
 	fi, err := os.Stat(path)
-	check(err)
+	if err != nil {
+		return fmt.Errorf("file doesn't exist: %v", err)
+	}
 	fileSize := fi.Size()
 
 	for range 3 {
@@ -25,25 +19,31 @@ func Shred(path string) error {
 		// If not could either choose to return error or use Chmod function
 		// Going to choose to just allow error for now
 		f, err := os.OpenFile(path, os.O_WRONLY, 0666)
-		check(err)
+		if (err != nil) {
+			return fmt.Errorf("couldn't open file for writing: %v", err)
+		}
 
 		// Write file with new data
 		buff := make([]byte, fileSize)
 		_, err = rand.Read(buff)
-		check(err)
+		if (err != nil) {
+			f.Close()
+			return fmt.Errorf("couldn't fill buffer with random data %v", err)
+		}
 
 		_, err = f.Write(buff)
-		check(err)
-
-		err = f.Close()
-		check(err)
+		f.Close()
+		if (err != nil) {
+			
+			return fmt.Errorf("couldn't write to file %v", err)
+		}
 	}
 	
 	// Now delete the file
 	err = os.Remove(path)
-	check(err)
-	
-	fmt.Println("Succesfully shredded: " + path)
+	if (err != nil) {
+		return fmt.Errorf("couldn't delete the file %v", err)
+	}
 
 	return err
 }
@@ -52,5 +52,9 @@ func main() {
 	// Extract path from arguments otherwise
 	path := "temp.txt" // Change to a generate file function
 	err := Shred(path)
-	check(err)
+	if err != nil {
+		fmt.Println("Error:", err)
+	} else {
+		fmt.Println("Succesfully shredded: " + path)
+	}
 }
